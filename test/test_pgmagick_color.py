2531878ce62dd53fb57de7a58ea16e9c6a55f027
hhatto <devnull@localhost>
2010-08-09 00:47:24 +0900

    added: module import tests

---
+++ test/test_pgmagick_color.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('../')
+sys.path.append('./')
+from pgmagick import Color
+from pgmagick import ColorHSL
+from pgmagick import ColorGray
+from pgmagick import ColorMono
+from pgmagick import ColorRGB
+from pgmagick import ColorYUV
