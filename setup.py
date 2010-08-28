6bf6e5093e5c566289001e817ba1de62ca749fe2
hhatto <devnull@localhost>
2010-08-28 17:06:47 +0900

    version 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ include_dirs = [get_python_inc(),
 library_dirs = ['/usr/lib']
 
 setup(name='pgmagick',
-    version="0.1.0",
+    version="0.1.1",
     description="Yet Another Python wrapper for GraphicsMagick++",
     long_description=open('README.rst').read(),
     author='Hideo Hattori',
