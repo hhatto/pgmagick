49f944deb2f09a6242405c0f9ee3dc27d094d90d
hhatto <devnull@localhost>
2010-08-28 16:18:47 +0900

    TypeMetric API's added

---
+++ src/_TypeMetric.cpp
@@ -0,0 +1,20 @@
+
+#include <boost/python.hpp>
+#include <boost/cstdint.hpp>
+
+#include <Magick++/TypeMetric.h>
+
+using namespace boost::python;
+
+void __TypeMetric()
+{
+    class_< Magick::TypeMetric, boost::noncopyable >("TypeMetric", init<  >())
+        .def("ascent", &Magick::TypeMetric::ascent)
+        .def("descent", &Magick::TypeMetric::descent)
+        .def("textWidth", &Magick::TypeMetric::textWidth)
+        .def("textHeight", &Magick::TypeMetric::textHeight)
+        .def("maxHorizontalAdvance", &Magick::TypeMetric::maxHorizontalAdvance)
+    ;
+
+}
+
