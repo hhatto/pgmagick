d6ecbdc7acbaf81a4ba5ac909c1bce51e9b23d01
hhatto <devnull@localhost>
2010-08-29 01:46:28 +0900

    added: QuantumType

---
+++ src/_QuantumType.cpp
@@ -0,0 +1,30 @@
+#include <boost/python.hpp>
+#include <boost/cstdint.hpp>
+
+#include <Magick++/Include.h>
+
+using namespace boost::python;
+
+
+void __QuantumType()
+{
+    enum_< Magick::QuantumType >("QuantumType")
+        .value("IndexQuantum", Magick::IndexQuantum)
+        .value("GrayQuantum", Magick::GrayQuantum)
+        .value("IndexAlphaQuantum", Magick::IndexAlphaQuantum)
+        .value("GrayAlphaQuantum", Magick::GrayAlphaQuantum)
+        .value("RedQuantum", Magick::RedQuantum)
+        .value("CyanQuantum", Magick::CyanQuantum)
+        .value("GreenQuantum", Magick::GreenQuantum)
+        .value("YellowQuantum", Magick::YellowQuantum)
+        .value("BlueQuantum", Magick::BlueQuantum)
+        .value("MagentaQuantum", Magick::MagentaQuantum)
+        .value("AlphaQuantum", Magick::AlphaQuantum)
+        .value("BlackQuantum", Magick::BlackQuantum)
+        .value("RGBQuantum", Magick::RGBQuantum)
+        .value("RGBAQuantum", Magick::RGBAQuantum)
+        .value("CMYKQuantum", Magick::CMYKQuantum)
+        .value("CIEYQuantum", Magick::CIEYQuantum)
+        .value("CIEXYZQuantum", Magick::CIEXYZQuantum)
+    ;
+}
