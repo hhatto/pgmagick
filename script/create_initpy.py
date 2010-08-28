6b4111e683ee3a4a585ad9e2bdb98e9fdc647089
hhatto <devnull@localhost>
2010-08-29 00:58:21 +0900

    added: __init__.py creation script.

---
+++ script/create_initpy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+usage:
+    $ cd pgmagick/pgmagick
+    $ python create_initpy.py > __init__.py
+"""
+import sys
+sys.path.append('.')
+import _pgmagick
+
+print "import _pgmagick\n\n"
+for obj in dir(_pgmagick):
+    if obj[:2] is '__':
+        continue
+    exec "typestr = str(type(_pgmagick.%s))" % obj
+    if typestr == "<type 'Boost.Python.class'>":
+        print "class %s(_pgmagick.%s):\n    pass\n" % (obj, obj)
+    elif typestr == "<type 'type'>":
+        print "%s = _pgmagick.%s\n" % (obj, obj)
