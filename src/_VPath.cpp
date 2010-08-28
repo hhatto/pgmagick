4751f32945562636f644f66a9608161e6b773fb6
hhatto <devnull@localhost>
2010-08-28 16:17:55 +0900

    Drawable.h API's added

---
+++ src/_VPath.cpp
@@ -0,0 +1,23 @@
+#include <boost/python.hpp>
+#include <boost/cstdint.hpp>
+
+#include <Magick++/Drawable.h>
+
+#include <Magick++.h>
+
+using namespace boost::python;
+
+
+void __VPath()
+{
+    class_< Magick::VPath >("VPath", init<  >())
+        .def(init< const Magick::VPathBase& >())
+        .def(init< const Magick::VPath& >())
+        .def( self == self )
+        .def( self != self )
+        .def( self > self )
+        .def( self < self )
+        .def( self >= self )
+        .def( self <= self )
+    ;
+}
