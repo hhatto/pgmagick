3d6d6278525a249d5a497da0aff88b56e8e43328
hhatto <devnull@localhost>
2010-09-12 01:43:01 +0900

    update: Drawable example.

--- example/draw.py
+++ example/draw.py
@@ -1,16 +1,13 @@
-from pgmagick import Image, Drawable, DrawableCircle, DrawableText, \
-                     Geometry, Color
+from pgmagick import Image, DrawableCircle, DrawableText, Geometry, Color
 
 im = Image(Geometry(300, 300), Color("yellow"))
 
 circle = DrawableCircle(100.0, 100.0, 20.0, 20.0)
-circle = Drawable(circle)
 im.draw(circle)
 
 im.fontPointsize(65)
 im.font("/var/lib/defoma/x-ttcidfont-conf.d/dirs/TrueType/UnBatang.ttf")
 text = DrawableText(30, 250, "hello gm")
-text = Drawable(text)
 im.draw(text)
 
 im.write('hoge.png')
