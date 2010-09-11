ddfc61789e58216a0b266ee2e5250ba281593bae
hhatto <devnull@localhost>
2010-09-11 15:55:46 +0900

    find to include file and library file.

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 from setuptools import setup, find_packages, Extension
 from distutils.sysconfig import get_python_inc
 import glob
+import os
 
-include_dirs = [get_python_inc(),
-                '/usr/include/GraphicsMagick/']
-library_dirs = ['/usr/lib']
+include_dirs = [get_python_inc()]
+library_dirs = []
+
+search_include_dirs = ['/usr/local/include/GraphicsMagick',
+                       '/usr/include/GraphicsMagick/']
+search_library_dirs = ['/usr/local/lib/', '/usr/lib/']
+
+
+def find_file(filename, search_dirs):
+    for dirname in search_dirs:
+        for root, dirs, files in os.walk(dirname):
+            for f in files:
+                if filename in f:
+                    return dirname
+            for d in dirs:
+                if filename in d:
+                    return dirname
+            if filename in root:
+                return dirname
+    return False
+
+include_dirs.append(find_file('Magick++', search_include_dirs))
+library_dirs.append(find_file('libGraphicsMagick', search_library_dirs))
 
 setup(name='pgmagick',
     version="0.2.1",
