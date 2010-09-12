a120f07bd981547f83e1c39c5cc7b135560d4dd9
hhatto <devnull@localhost>
2010-09-13 00:22:37 +0900

    implement CoordinateList

--- src/_Drawable.cpp
+++ src/_Drawable.cpp
@@ -37,4 +37,15 @@ void __Drawable()
         .def("count", &std::list<Magick::Drawable>::size)
         .def("__len__", &std::list<Magick::Drawable>::size)
     ;
+    class_< Magick::CoordinateList >("CoordinateList", init<  >())
+        .def(init< const Magick::CoordinateList& >())
+        .def("push_back", &std::list<Magick::Coordinate>::push_back)
+        .def("append", &std::list<Magick::Coordinate>::push_back)
+        .def("pop_back", &std::list<Magick::Coordinate>::pop_back)
+        .def("pop", &std::list<Magick::Coordinate>::pop_back)
+        .def("remove", &std::list<Magick::Coordinate>::remove)
+        .def("reverse", &std::list<Magick::Coordinate>::reverse)
+        .def("count", &std::list<Magick::Coordinate>::size)
+        .def("__len__", &std::list<Magick::Coordinate>::size)
+    ;
 }
