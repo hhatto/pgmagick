3ea768108c8a7aac2af9896b9c71a1771968c77a
hhatto <devnull@localhost>
2010-10-01 01:20:36 +0900

    added unittest

--- test/test_pgmagick_image.py
+++ test/test_pgmagick_image.py
@@ -1,4 +1,23 @@
 import sys
+import unittest
 sys.path.append('../')
 sys.path.append('./')
-from pgmagick import Image
+from pgmagick import Image, Geometry, Color
+
+
+class TestImage(unittest.TestCase):
+
+    def test_noarg_init(self):
+        im = Image()
+        self.assertEqual(type(im), Image)
+
+    def test_fromfile_init_error(self):
+        self.assertRaises(RuntimeError, Image, "xXxX.jpg")
+
+    def test_size_and_color_init(self):
+        im = Image(Geometry(300, 200), Color('transparent'))
+        size = im.size()
+        self.assertEqual(300, size.width())
+        self.assertEqual(200, size.height())
+
+unittest.main()
