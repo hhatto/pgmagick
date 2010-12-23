bd52b17b75ba11a4c20fa7e893b4e21097a48ccb
hhatto <devnull@localhost>
2010-12-23 18:11:29 +0900

    version 0.3.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ include_dirs.append(find_file('Magick++', search_include_dirs))
 library_dirs.append(find_file('libGraphicsMagick', search_library_dirs))
 
 setup(name='pgmagick',
-    version="0.3.1",
+    version="0.3.2",
     description="Yet Another Python wrapper for GraphicsMagick",
     long_description=open('README.rst').read(),
     author='Hideo Hattori',
