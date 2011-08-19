5a4804753fdd0f56502864dffe05a38acd6d1f25
hhatto <devnull@localhost>
2011-08-19 23:03:00 +0900

    update

--- example/imagelist.py
+++ example/imagelist.py
@@ -3,7 +3,7 @@ from pgmagick import Image, ImageList, Geometry, Color
 imgs = ImageList()
 for color in ('red', 'blue', 'green', 'black', 'yellow'):
     imgs.append(Image(Geometry(200, 200), Color(color)))
-imgs.animationDelayImage(100)
-imgs.scaleImage(Geometry(100, 100))
+imgs.animationDelayImages(100)
+imgs.scaleImages(Geometry(100, 100))
 print len(imgs)
 imgs.writeImages('output.gif')
