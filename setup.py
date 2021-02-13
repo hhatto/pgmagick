642924355faf395333a7035488b4ba7beb8d526e
Hideo Hattori <hhatto.jp@gmail.com>
2021-02-13 13:31:15 +0900

    fix

--- setup.py
+++ setup.py
@@ -136,6 +136,7 @@ _python_version = sys.version_info
 boost_lib_target_files = []
 if _python_version >= (3, ):
     boost_lib_target_files.append("boost_python-py%s%s" % (_python_version[0], _python_version[1]))
+    boost_lib_target_files.append("boost_python%s%s" % (_python_version[0], _python_version[1]))
     # ArchLinux uses boost_python3
     boost_lib_target_files.append("boost_python3")
     boost_lib_target_files.append("boost_python3-mt")
@@ -150,6 +151,7 @@ boost_lib_target_files.append("boost_python%s%s-mt" % (_python_version[0], _pyth
 for boost_lib in boost_lib_target_files:
     lib_path = find_file('lib%s.' % boost_lib, search_library_dirs)
     if lib_path:
+        print("lib_path: {}".format(lib_path))
         break
 
 if not lib_path:
